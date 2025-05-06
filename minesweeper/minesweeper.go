e157e5226d28b5229b9593f7b8b2bcb8a47fe305
Yohan DUFAUD <ydufaud@sm-adn.fr>
2025-05-06 12:00:12 +0200

    update

--- minesweeper/minesweeper.go
+++ minesweeper/minesweeper.go
@@ -1,60 +1,36 @@
 package minesweeper
 
 import (
-	"errors"
-	"regexp"
-	"strings"
+	"fmt"
 )
 
-type position struct{ row, col uint }
-
-//Count generates the solution
-func (b *Board) Count() error {
-	initialRe := regexp.MustCompile(`^\+[-]*\+$`)
-	centralRe := regexp.MustCompile(`^\|[ \*]*\|$`)
-	rows := strings.Split(b.String(), "\n")[1:]
-
-	var size = 0
-	for i, row := range rows {
-		if i == 0 {
-			size = len(row)
-		} else {
-			if len(row) != size {
-				return errors.New("Invalid board cause all rows are not equal")
+func Annotate(mines []string) []string {
+	nbBombs := func(r, c int) string {
+		bombs := 0
+		adjacents := [][]int{{r - 1, c - 1}, {r - 1, c}, {r - 1, c + 1}, {r, c - 1}, {r, c + 1}, {r + 1, c - 1}, {r + 1, c}, {r + 1, c + 1}}
+		for _, a := range adjacents {
+			if a[0] >= 0 && a[0] < len(mines) && a[1] >= 0 && a[1] < len(mines[0]) && string(mines[a[0]][a[1]]) == "*" {
+				bombs++
 			}
 		}
-		if i == 0 || i == len(rows)-1 {
-			if !initialRe.MatchString(row) {
-				return errors.New("Invalid board cause first or last row is not valid")
-			}
-		} else {
-			if !centralRe.MatchString(row) {
-				return errors.New("Invalid board cause one line is not valid")
-			}
+		if bombs == 0 {
+			return " "
 		}
+		return fmt.Sprintf("%d", bombs)
 	}
 
-	for r := 1; r < len(*b)-1; r++ {
-		for c := 1; c < size-1; c++ {
-			if (*b)[r][c] != byte('*') {
-				var bombs uint8 = 0
-				for i := c - 1; i <= c+1; i++ {
-					if (*b)[r-1][i] == byte('*') {
-						bombs++
-					}
-					if (*b)[r+1][i] == byte('*') {
-						bombs++
-					}
-					if (*b)[r][i] == byte('*') && i != c {
-						bombs++
-					}
-				}
-				if bombs != 0 {
-					(*b)[r][c] = byte(48 + bombs)
-				}
+	annotated := make([]string, len(mines))
+	for r, row := range mines {
+		newRow := ""
+		for c, char := range row {
+			if string(char) == "*" {
+				newRow += "*"
+
+			} else {
+				newRow += nbBombs(r, c)
 			}
 		}
+		annotated[r] = newRow
 	}
-
-	return nil
+	return annotated
 }
